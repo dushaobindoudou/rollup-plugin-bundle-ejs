877b95759268e295a9c6bbc04d22b09eea287585
dushaobin <dushaobin@meishubao.com>
2019-09-28 11:29:43 +0800

    fewfaef

--- rollup.config.js
+++ rollup.config.js
@@ -4,15 +4,8 @@ import buble from 'rollup-plugin-buble';
 const pkg = require('./package.json');
 
 export default {
-  input: 'src/index.js',
-  output: [
-    { format: 'cjs', file: pkg['main'] },
-    { format: 'es', file: pkg['module'] }
-  ],
-  external: ['fs', 'path', 'crypto'],
-  plugins: [
-    resolve(),
-    commonjs(),
-    buble()
-  ]
+    input: 'src/index.js',
+    output: [{ format: 'cjs', file: pkg['main'] }, { format: 'es', file: pkg['module'] }],
+    external: ['fs', 'path'],
+    plugins: [resolve(), commonjs(), buble()],
 };
