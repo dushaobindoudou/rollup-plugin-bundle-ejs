74251af9b538176a7938c4f85e6ecacb279dc782
dushaobin <dushaobin@meishubao.com>
2019-09-30 15:36:54 +0800

    fix: 修复rollup配置

--- rollup.config.js
+++ rollup.config.js
@@ -1,11 +1,18 @@
 import commonjs from 'rollup-plugin-commonjs';
 import resolve from 'rollup-plugin-node-resolve';
 import buble from 'rollup-plugin-buble';
+
 const pkg = require('./package.json');
 
 export default {
     input: 'src/index.js',
-    output: [{ format: 'cjs', file: pkg['main'] }, { format: 'es', file: pkg['module'] }],
+    output: [{ format: 'cjs', file: pkg.main }, { format: 'es', file: pkg.module }],
     external: ['fs', 'path'],
-    plugins: [resolve(), commonjs(), buble()],
+    plugins: [
+        resolve(),
+        commonjs({
+            ignore: ['conditional-runtime-dependency'],
+        }),
+        buble(),
+    ],
 };
